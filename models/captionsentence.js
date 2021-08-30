6224176a58b4ac41b233d9d24c99ac36f16c8bbd
Adam <achappell2001@gmail.com>
2021-08-30 13:50:15 +1200

    Sequelize Models

---
+++ models/captionsentence.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CaptionSentence extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({CaptionFile, Edit}) {
+      // define association here
+      this.belongsTo(CaptionFile)
+      this.hasMany(Edit)
+    }
+  };
+  CaptionSentence.init({
+    position: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    start: {
+      type: DataTypes.STRING(8),
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING(200)
+    }
+  }, {
+    sequelize,
+    modelName: 'CaptionSentence',
+  });
+  return CaptionSentence;
+};
