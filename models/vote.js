29e1f1281980d2c787da804cb7b63ba133d88b46
Adam <achappell2001@gmail.com>
2021-08-31 12:14:07 +1200

    Tracking of user voting and reporting

---
+++ models/vote.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Vote extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({User, Edit}) {
+      // define association here
+      this.belongsTo(User)
+      this.belongsTo(Edit)
+    }
+  };
+  Vote.init({
+    upvoted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Vote',
+  });
+  return Vote;
+};
