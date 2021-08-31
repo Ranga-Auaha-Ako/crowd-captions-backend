b609e3069968192868fc8b8586245fdcdfb14e8d
Adam <achappell2001@gmail.com>
2021-08-31 13:15:59 +1200

    CaptionSentence start time changed to double  datatype

--- models/captionsentence.js
+++ models/captionsentence.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
     },
     start: {
-      type: DataTypes.TIME,
+      type: DataTypes.DOUBLE,
       allowNull: false
     },
     body: {
