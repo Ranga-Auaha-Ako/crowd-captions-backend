5f568dc9528f7212d1e0b01780cb6600aa82e9da
Adam <achappell2001@gmail.com>
2021-09-13 22:43:32 +1200

    Validation functions for api requests

---
+++ backend/src/utilities/validation.js
@@ -0,0 +1,10 @@
+const validator = require("validator")
+
+module.exports = {
+    checkLectureID: function (lectureID) {
+        return validator.isUUID(lectureID)
+    },    
+    checkEditSize: function (edit) {
+        return validator.isLength(edit, {min: 0, max: 200})
+    }
+}
