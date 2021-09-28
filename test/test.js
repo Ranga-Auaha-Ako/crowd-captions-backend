685ed307f216c419874ed085d024186c8ba4b755
Adam <achappell2001@gmail.com>
2021-09-28 18:11:28 +1300

    Test templating

--- test/test.js
+++ test/test.js
@@ -1,9 +1,30 @@
 var assert = require('assert')
+const {sequelize, User, Vote, Edit} = require("../models")
 
-describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal([1, 2, 3].indexOf(4), -1)
+describe('Get Captions', async () => {
+    describe('Lecture Id does not exists', async () => {
+        
+        it('Return Empty json file', async () => {  
+            await sequelize.sync({force: true})
+            assert.equal(true, true)
+        })
+        it('Not add lecture id to database', async () => {  
+            assert.equal(true, true)
+        })
+    })
+
+    describe('Lecture Id exists, but captions has not been grabbed before()', () => {
+        it('Return data as specified (expand)', () => {
+            assert.equal(true, true)
+        })
+    })
+
+    describe('Lecture Id exists and caption has been grabbed before', () => {
+        it('Return data as specified (expand)', () => {
+            assert.equal(true, true)
+        })
+        it('Include existing edits', () => {
+            assert.equal(true, true)
         })
     })
 })
