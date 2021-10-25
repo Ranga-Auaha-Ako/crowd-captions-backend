4fe5937736c75bc1d13d85303c8ba7cbb6f6d6a0
Zac Miller-Waugh <z.millerwaugh@auckland.ac.nz>
2021-10-25 13:38:40 +1300

    Fix production configuration

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -13,15 +13,15 @@ module.exports = {
         panopto_clientSecret: process.env.panopto_clientSecret
     },
     production: {
-        database: {
-            rest: {
-                database: process.env.POSTGRES_DB,
-                username: process.env.POSTGRES_USER,
-                password: process.env.POSTGRES_PASS,
-                host: process.env.POSTGRES_HOST,
-                port: process.env.POSTGRES_PORT,
-                dialect: 'postgres'
-            },
-        },
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASS,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        dialect: 'postgres',
+        panopto_username: process.env.panopto_username,
+        panopto_password: process.env.panopto_password,
+        panopto_clientId: process.env.panopto_clientId,
+        panopto_clientSecret: process.env.panopto_clientSecret
     },
 };
