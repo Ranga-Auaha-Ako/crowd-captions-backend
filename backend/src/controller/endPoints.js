6625e1cdbde112dc74b0bcd5c463f79bf2179110
Zac M-W <z.millerwaugh@auckland.ac.nz>
2021-11-30 18:07:23 +1300

    Remove unnecessary headers

--- backend/src/controller/endPoints.js
+++ backend/src/controller/endPoints.js
@@ -71,17 +71,6 @@ export const getCaptions = async(lectureId, upi) => {
             method: "get",
             url: `https://${panoptoEndpoint}/Panopto/Pages/Transcription/GenerateSRT.ashx?id=${lectureId}&language=1`,
             headers: {
-              authority: panoptoEndpoint,
-              "cache-control": "max-age=0",
-              "sec-ch-ua-mobile": "?0",
-              "upgrade-insecure-requests": "1",
-              accept:
-                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-              "sec-fetch-site": "none",
-              "sec-fetch-mode": "navigate",
-              "sec-fetch-user": "?1",
-              "sec-fetch-dest": "document",
-              "accept-language": "en-US,en;q=0.9",
               cookie: `${cookie1} ${cookie2}`,
             },
           };
