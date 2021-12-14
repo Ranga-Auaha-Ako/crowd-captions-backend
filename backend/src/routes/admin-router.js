e2e2decd07e8035dd3e7839b14a42c53afe43417
HaoLI-jpg <lih1015132136@gmail.com>
2021-12-14 15:39:24 +1300

    added resources to admin bro

--- backend/src/routes/admin-router.js
+++ backend/src/routes/admin-router.js
@@ -1,11 +1,15 @@
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
+const AdminBroSequelize = require('@admin-bro/sequelize')
+
 
 const express = require('express')
 const app = express()
+const db = require("../models");
 
+AdminBro.registerAdapter(AdminBroSequelize)
 const adminBro = new AdminBro({
-  databases: [],
+  databases: [db],
   rootPath: '/admin',
 })
 
