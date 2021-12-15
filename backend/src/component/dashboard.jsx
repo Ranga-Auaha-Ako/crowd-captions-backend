1c505efb786b9f804a844ee998f763eebd45cbe7
HaoLI-jpg <lih1015132136@gmail.com>
2021-12-16 01:50:58 +1300

    trying to modify the admin page

---
+++ backend/src/component/dashboard.jsx
@@ -0,0 +1,24 @@
+import { ApiClient } from 'admin-bro'
+import { Box } from '@admin-bro/design-system'
+import { useState, useEffect } from 'react'
+const api = new ApiClient()
+
+const Dashboard = () => {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    api.getDashboard().then((response) => {
+      setData(response.data)
+    })
+  }, [])
+
+  return (
+    <Box variant="grey">
+      <Box variant="white">
+        <h1>hello world</h1>
+      </Box>
+    </Box>
+  )
+}
+
+export default Dashboard
